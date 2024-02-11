d877e5e02571a26b82fa2c8f759aa8e65c8eba96
Gabriel Freitas <gabrielcenteio.freitas@gmail.com>
2024-02-11 02:14:21 -0300

    :tada: Color Randomizar

---
+++ script.js
@@ -0,0 +1,21 @@
+function changeColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    $("body").css("background-color", color);
+    $("#codigo_cor").text(color);
+    return color
+}
+
+function copyText() {
+    let textoCopiado = document.getElementById("codigo_cor");
+        let range = document.createRange();
+        range.selectNodeContents(textoCopiado);
+        window.getSelection().removeAllRanges();
+        window.getSelection().addRange(range);
+        document.execCommand("copy");
+        window.getSelection().removeAllRanges();
+        alert('Código da cor copiado!')
+}
